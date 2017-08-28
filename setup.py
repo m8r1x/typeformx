20d70a0365b25e15525e203b9da57c47ff324643
m8r1x <m8r1x.io@gmail.com>
2017-08-28 14:02:17 +0300

    update setup configuration

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='TypeformX',
     version='0.1dev',
-    packages=['typeformx'],
+    packages=find_packages(),
+    include_package_data=True,
     license='MIT',
+    install_requires=[
+        'pandas',
+        'requests',
+        'textblob',
+        'textract'
+    ],
+    test_require=[
+        'mock',
+        'nose'
+    ]
     
     long_description=open('README.md').read()
 )
